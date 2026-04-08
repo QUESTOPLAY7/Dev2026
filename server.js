6e1bb7a8d6b2e666ed3970821c9e35e80d9249ce
QUESTOPLAY7 <104244970+QUESTOPLAY7@users.noreply.github.com>
2026-04-08 23:51:34 +0100

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // [ THE ARCHITECT ] - THE INFINITE ILLUSION ENGINE
-const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
+const wss = new
+WebSocket.Server({ port:
+process.env.PORT || 8080 });
 const SIGNAL_SEPARATOR = '\x1e';
 
 console.log('[SYSTEM] السيرفر الأوتوماتيكي شاعل... كيتسنى الضحايا.');
